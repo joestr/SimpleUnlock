c54d52176a0c8b5f103488fdbb44ddffce90120e
joestr <strasser999@gmail.com>
2018-09-09 11:58:08 +0200

    Made 1.13 compatible

--- src/main/java/net/dertod2/SimpleUnlock/Binary/SimpleUnlock.java
+++ src/main/java/net/dertod2/SimpleUnlock/Binary/SimpleUnlock.java
@@ -16,33 +16,33 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleUnlock extends JavaPlugin {
-	private static SimpleUnlock simpleUnlock;
-	
-	public static UnlockControl unlockControl;
-	
-	public static Map<UUID, Unlock> unlockList = new HashMap<UUID, Unlock>();	
-	public static Map<UUID, Long> declineList = new HashMap<UUID, Long>();
-	
-	public void onEnable() {
-		this.saveDefaultConfig();
-		
-		SimpleUnlock.simpleUnlock = this;
-		
-		SimpleUnlock.unlockControl = new UnlockControl();
-		SimpleUnlock.unlockControl.load();
-		
-		if (getConfig().getBoolean("deny-guest-actions", true)) {
-			PluginManager pluginManager = Bukkit.getPluginManager();
-			pluginManager.registerEvents(new BlockListener(), this);
-			pluginManager.registerEvents(new EntityListener(), this);
-			pluginManager.registerEvents(new PlayerListener(), this);
-		}
-		
-		getCommand("unlock").setExecutor(new UnlockCommand());
-		getCommand("unlockadmin").setExecutor(new UnlockAdminCommand());
-	}
-	
-	public static FileConfiguration getConfiguration() {
-		return SimpleUnlock.simpleUnlock.getConfig();
-	}
+    private static SimpleUnlock simpleUnlock;
+
+    public static UnlockControl unlockControl;
+
+    public static Map<UUID, Unlock> unlockList = new HashMap<UUID, Unlock>();
+    public static Map<UUID, Long> declineList = new HashMap<UUID, Long>();
+
+    public void onEnable() {
+        this.saveDefaultConfig();
+
+        SimpleUnlock.simpleUnlock = this;
+
+        SimpleUnlock.unlockControl = new UnlockControl();
+        SimpleUnlock.unlockControl.load();
+
+        if (getConfig().getBoolean("deny-guest-actions", true)) {
+            PluginManager pluginManager = Bukkit.getPluginManager();
+            pluginManager.registerEvents(new BlockListener(), this);
+            pluginManager.registerEvents(new EntityListener(), this);
+            pluginManager.registerEvents(new PlayerListener(), this);
+        }
+
+        getCommand("unlock").setExecutor(new UnlockCommand());
+        getCommand("unlockadmin").setExecutor(new UnlockAdminCommand());
+    }
+
+    public static FileConfiguration getConfiguration() {
+        return SimpleUnlock.simpleUnlock.getConfig();
+    }
 }
--- src/main/java/net/dertod2/SimpleUnlock/Listeners/PlayerListener.java
+++ src/main/java/net/dertod2/SimpleUnlock/Listeners/PlayerListener.java
@@ -19,70 +19,72 @@ import org.bukkit.event.player.PlayerPickupItemEvent;
 import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
+@SuppressWarnings("deprecation")
 public class PlayerListener implements Listener {
 
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerInteract(PlayerInteractEvent event) {
-		PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
-		if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
-			event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
-			event.setCancelled(true);
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-		PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
-		if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
-			event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
-			event.setCancelled(true);
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
-		PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
-		if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
-			event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
-			event.setCancelled(true);
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerBucketFill(PlayerBucketFillEvent event) {
-		PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
-		if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
-			event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
-			event.setCancelled(true);
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerPickupItem(PlayerPickupItemEvent event) {
-		PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
-		if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
-			//event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
-			event.setCancelled(true);
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
-		PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
-		if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
-			event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
-			event.setCancelled(true);
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerLogin(PlayerJoinEvent event) {
-		PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
-		if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
-			List<String> declineList = SimpleUnlock.getConfiguration().getStringList("welcome-message");
-			for (String message : declineList) {
-				event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-			}
-		}
-	}
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
+        if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
+            event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
+        if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
+            event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
+        PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
+        if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
+            event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerBucketFill(PlayerBucketFillEvent event) {
+        PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
+        if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
+            event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+        PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
+        if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
+            // event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein
+            // um diese Aktion auszuführen!");
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+        PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
+        if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
+            event.getPlayer().sendMessage(ChatColor.RED + "Du musst freigeschaltet sein um diese Aktion auszuführen!");
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerLogin(PlayerJoinEvent event) {
+        PermissionUser permissionUser = PermissionsEx.getUser(event.getPlayer());
+        if (!permissionUser.inGroup(SimpleUnlock.getConfiguration().getString("player-group"))) {
+            List<String> declineList = SimpleUnlock.getConfiguration().getStringList("welcome-message");
+            for (String message : declineList) {
+                event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+            }
+        }
+    }
 }
